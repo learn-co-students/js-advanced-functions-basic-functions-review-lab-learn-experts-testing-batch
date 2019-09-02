f8aaf4ea5ece0947c6720fe347e8f391d3adb956
github username <github email address>
2019-09-01 23:14:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+/// For hoisting to work, the function must be defined as a function declaration. ///
+
+/// function expressions are not hoisted
+let mondayWork = function(str = "go to the office"){
+  return `This Monday, I will ${str}.`
+}
+
+function wrapAdjective(str = "*"){
+  return function(string = "special"){
+    return `You are ${str}${string}${str}!`
+  }
+}
+
+const Calculator = {
+  add: function(n1, n2) { return n1 + n2 },
+  subtract: function(n1, n2) { return n1 - n2 },
+  multiply: function(n1, n2) { return n1 * n2 },
+  divide: function(n1, n2) { return n1 / n2 }
+}
+
+function actionApplyer(start, fnArray){
+  if (fnArray.length === 0) return start
+  let result = start
+  fnArray.forEach(fn => result = fn(result))
+  return result
+}
